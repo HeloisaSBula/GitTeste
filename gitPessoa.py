5ed0fcc33b31d42ce5144b1aff2e50f86efa2861
HeloisaSBula <heloisa.bula@shift.com.br>
2025-03-14 16:13:17 -0300

    Novo branch

--- gitPessoa.py
+++ gitPessoa.py
@@ -1,6 +1,6 @@
 print("Agora eu sei o básico dos comandos")
 print("Tenho que praticar")
 
-print("Essa mudança ainda não foi commitada")
-
 print("Se eu commitar, ele não mostra mais a diferença")
+
+print("Essa mudança foi no novo branch")
